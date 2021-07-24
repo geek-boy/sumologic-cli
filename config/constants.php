6a15b8b860f61332dce78d5dcb3e46ccea8e4ad4
Geek Boy <github@geekboy.com.au>
2021-07-24 17:46:40 +1000

    Moved constants into 'config/constants.php'

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+use Symfony\Component\Console\Helper\ProgressBar;
+
+define("DEFAULT_CRED_FILE_PATH",getenv("HOME") . "/.sumologic-creds.yml");
+define("SUMOLOGIC_JOB_SEARCH_API","https://api.sumologic.com/api/v1/search/jobs");
+define("DEFAULT_RESULTS_DIR_PATH",getenv("HOME"));
+
+ProgressBar::setFormatDefinition('request_query_record_progress', 'Getting records... %recordCount% to %upperLimit%');
+ProgressBar::setFormatDefinition('request_query_file_size_progress', 'File size is %logFileSize%');
+ProgressBar::setFormatDefinition('api_controller_downloaded_bytes', '%date%: Downloading - Bytes downloaded %downloadedBytes%');
